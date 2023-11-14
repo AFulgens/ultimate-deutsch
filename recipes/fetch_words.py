97ea4f0bc19bd1050065b4abe0ebd40cff40e63d
Dániel Kovács <daniel@kovacs.xyz>
2023-11-14 17:51:11 +0100

    Add words: A1, verbs r*

--- recipes/fetch_words.py
+++ recipes/fetch_words.py
@@ -5,8 +5,7 @@ import urllib.request
 from bs4 import BeautifulSoup
 from merge_and_sort import sort_by_caseless_columns
 
-
-columns = [
+verb_columns = [
     "praesens_singular_1",
     "praesens_singular_2",
     "praesens_singular_3",
@@ -55,7 +54,7 @@ def fetch_klass(word):
 def fetch_verb_from_duden(wort, link):
     site = urllib.request.urlopen(link).read()
 
-    column_iter = iter(columns)
+    column_iter = iter(verb_columns)
     new_verb = pd.DataFrame([{"wort": wort}])
 
     anchors = [
@@ -66,23 +65,23 @@ def fetch_verb_from_duden(wort, link):
     ]
 
     for anchor in anchors:
-        current = iter(BeautifulSoup(site, features="html.parser") \
-                        .find_all(attrs={"class": "accordion__item"}, string=anchor[0])[anchor[1]] \
-                        .parent \
-                        .children)
+        current = iter(BeautifulSoup(site, features="html.parser")
+                       .find_all(attrs={"class": "accordion__item"}, string=anchor[0])[anchor[1]]
+                       .parent
+                       .children)
         next(current)
         for entry in current:
             new_verb[next(column_iter)] = entry.contents[0]
 
     imperativ = \
         BeautifulSoup(site, features="html.parser") \
-                   .find_all(attrs={"class": "accordion-tables__header"}, string="Imperativ")[0] \
-                   .next_sibling.next_sibling.div.div.div.ul.next_sibling
+            .find_all(attrs={"class": "accordion-tables__header"}, string="Imperativ")[0] \
+            .next_sibling.next_sibling.div.div.div.ul.next_sibling
     new_verb[next(column_iter)] = imperativ.li.contents[0]
     new_verb[next(column_iter)] = imperativ.li.next_sibling.contents[0]
 
     infinite = \
-    BeautifulSoup(site, features="html.parser") \
+        BeautifulSoup(site, features="html.parser") \
             .find_all(attrs={"class": "accordion-tables__header"}, string="Infinite Formen")[0] \
             .next_sibling.next_sibling.div
     new_verb[next(column_iter)] = infinite.div.div.ul.li.next_sibling.contents[0]
@@ -90,7 +89,7 @@ def fetch_verb_from_duden(wort, link):
     new_verb[next(column_iter)] = infinite.div.next_sibling.next_sibling.div.ul.li.next_sibling.contents[0]
 
     aux_verb = BeautifulSoup(site, features="html.parser") \
-            .find(string=re.compile(".*Perfektbildung mit „.*"))
+        .find(string=re.compile(".*Perfektbildung mit „.*"))
     if aux_verb is None:
         new_verb[next(column_iter)] = "TODO"
     else:
@@ -111,6 +110,7 @@ def fetch_verb_into(wort, link, verbs):
         new_verb = fetch_verb_from_wiktionary(wort, link)
 
     if new_verb is not None:
+        new_verb = new_verb.replace({"–": None})
         return pd.concat([verbs, new_verb], ignore_index=True)
 
 
